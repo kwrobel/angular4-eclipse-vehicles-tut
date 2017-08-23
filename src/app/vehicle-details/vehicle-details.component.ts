f2de72b1da34365160ab64c2909219fcbe0635e5
Kay Wrobel <kay.wrobel@gmx.net>
2017-08-23 11:07:07 -0500

    First commit

---
+++ src/app/vehicle-details/vehicle-details.component.ts
@@ -0,0 +1,19 @@
+import { Vehicle } from '../model/vehicle';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-details',
+  templateUrl: './vehicle-details.component.html',
+  styleUrls: ['./vehicle-details.component.css']
+})
+export class VehicleDetailsComponent implements OnInit {
+
+  @Input()
+  vehicle: Vehicle;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
